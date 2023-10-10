3414788193a6500b24ce155145cb3aa90647784a
Ng Jia Shiang <ngjiashiang123@gmail.com>
2023-10-11 00:33:37 +0800

    sort robots by created_at desc

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
 {
     public function getDashboard(): Response
     {
-        $robots = Robot::with('latestStatus')->get();
+        $robots = Robot::with('latestStatus')->orderBy('created_at', 'desc')->get();
 
         return Inertia::render('Dashboard', [
             'robots' => $robots,
