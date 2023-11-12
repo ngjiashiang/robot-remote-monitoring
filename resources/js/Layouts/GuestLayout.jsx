193937edb34689358decc91400619cd8b09d6522
Ng Jia Shiang <ngjiashiang123@gmail.com>
2023-11-12 12:29:35 +0800

    on login full reload, app name on login page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -10,13 +10,16 @@ export default function Guest({ children }) {
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-32x32.png" />
             </Head>
             <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+                <div className='mb-4 text-2xl font-bold'>
+                    watchBot
+                </div>
                 <div>
                     <a href="/">
                         <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                     </a>
                 </div>
 
-                <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <div className="w-full sm:max-w-md mt-6 px-6 py-4overflow-hidden sm:rounded-lg">
                     {children}
                 </div>
             </div>
